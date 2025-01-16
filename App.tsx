951ec3f32d5b50e1233688902bfa15b2ba69ddb2
DionEmary <emarydion15@gmail.com>
2025-01-16 09:14:30 -0700

    added work done during the lab

--- App.tsx
+++ App.tsx
@@ -1,10 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import vacationDestination , { vacationPricing } from './app/vacation';
 
 export default function App() {
+  type Person = {
+    name: string;
+    age: number;
+    isStudent?: boolean;
+    height: number;
+  };
+
+  interface Person2 {
+    name: string;
+    occupation: string;
+    vacationDestination?: string;
+    vacationPricing?: string;
+  }
+
+  const name: string = "John Doe";
+  let occupation: string = "Software Developer";
+
+  const isStudent: boolean = true;
+  const age: number = 25
+  const height: number = 5.11;
+  const data: Person = {
+    name: "John Doe",
+    age: 25,
+    isStudent: true,
+    height: 5.11
+  };
+
+  const data2: Person = {
+    name, age, isStudent, height
+  }
+
+  const data3: Person2 = {
+    name: "Dion",
+    occupation: "Student and Line Cook",
+    vacationDestination: vacationDestination('Dubai'),
+    vacationPricing: vacationPricing(1000)
+  }
+
+  const datalist = [data, data2];
+
+  console.log(datalist[0].name);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View>
+        <Text style={styles.Title}>
+          Welcome {datalist[0].name}
+        </Text>
+      </View>
+      <View>
+        <Text>
+          Vacation Destination: {data3.vacationDestination}
+        </Text>
+        <Text>
+        Vacation Price: {data3.vacationPricing}
+        </Text>
+      </View>
+      <View>
+        <TouchableOpacity onPress={() => {alert("Vacation Ready")}}>
+          <Text>Click Me</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +77,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Title: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
 });
---
+++ app/vacation.ts
@@ -0,0 +1,7 @@
+export default function vacationDestination(place: string) {
+    return `Let's go to ${place}`;
+}
+
+export const vacationPricing = (price: number) => {
+    return `Vacation pricing is ${price}`;
+}
